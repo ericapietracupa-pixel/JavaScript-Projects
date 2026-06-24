185fbd6709d1de55d8c2877736ba2211b155e626
ericapietracupa-pixel <erica.pietracupa@gmail.com>
2026-06-24 15:54:58 -0400

    Updated README file for JavaScript projects - also updated portfolio project with Auto matic slide show transition function

--- Erica-Pietracupa-Portfolio-Website/js/portfolio.js
+++ Erica-Pietracupa-Portfolio-Website/js/portfolio.js
@@ -7,9 +7,10 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
-// This function displays the first image in the slideshow when the page loads
-var slideIndex = 1;
-showSlides(slideIndex);
+// This function displays the first image in the slideshow when the page loads, 
+// it then automatically progresses slideshow every 10 seconds
+var slideIndex = 0;
+showSlidesAuto();
 
 // This function changes the slide when the left or right arrows are clicked
 function plusSlides(n) {
@@ -23,11 +24,11 @@ function currentSlide(n) {
 
 
 function showSlides(n) {
+    let i;
     var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
-
     var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
-    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
-    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+    if (n > slides.length) { slideIndex = 1 }; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+    if (n < 1) { slideIndex = slides.length }; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
     }
@@ -38,11 +39,30 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
 }
 
+//This function automatically changes the slide displayed every 10 seconds
+function showSlidesAuto() {
+    let i;
+    var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
+    var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
+    }
+    slideIndex++; // Increment slideIndex
+    if (slideIndex > slides.length) { slideIndex = 1 } // If slideIndex is greater than the length of the array "slides", the slideIndex is set to 1
+    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
+    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+
+    setTimeout(showSlidesAuto, 10000); // Change image every 10 seconds
+}
+
 // This code will create close the contact form when the user clicks off of it
 // The first step is to add an event listener for any clicks on the website
-document.addEventListener("click", function(event){
+document.addEventListener("click", function (event) {
     // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form AND the click does not happen on any element with the contact class then call the closeForm() function
-    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")) {
         closeForm()
     }
-}, false )
+}, false)
--- README.md
+++ README.md
@@ -3,30 +3,38 @@ This repository is designed to house my JavaScript projects and assignments for 
 
 ## Projects
 - [AJAX](#ajax)
-- [Basic_JavaScript_Projects](#basic_javascript_projects)
+- [Basic_JavaScript_Projects](#basic-javascript-projects)
 - [Calculator](#calculator)
 - [Erica Pietracupa Portfolio Website](#erica-pietracupa-portfolio-website)
 - [JQuery](#jquery)
 - [Pizza Project](#pizza-project)
 - [React Projects](#react-projects)
-- [Tic Tac Toe](#tic_tac_toe)
+- [Tic-Tac-Toe](#tic-tac-toe)
 - [Todo App](#todo-app)
 
-## [AJAX](https://github.com/ericapietracupa-pixel/)
+### [AJAX](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/AJAX)
+Simple ajax project demonstrating how an ajax call can return content from another site/page and display that pulled content onto a webpage.
 
-## [Basic_JavaScript_Projects](https://github.com/ericapietracupa-pixel/)
+### [Basic JavaScript Projects](http://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/Basic%20JavaScript%20Projects)
+Various simple JavaScript projects demonstrating how to use different JavaScript functions and features including: alets, math operators, loops and arrays, string methods, comparisons, timers, etc.
 
-## [Calculator](https://github.com/ericapietracupa-pixel/)
+### [Calculator](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/Calculator)
+A basic calculator where users can add, subtract, divide and multiply numbers together
 
-## [Erica Pietracupa Portfolio Website](https://github.com/ericapietracupa-pixel/)
+### [Erica Pietracupa Portfolio Website](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/Erica-Pietracupa-Portfolio-Website)
+My portforlio website upgraded from the CSS and Bootstrap course to include a JavaScript run SlideShow feature and a pop-up contact form.
 
-## [JQuery](https://github.com/ericapietracupa-pixel/)
+### [JQuery](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/jQuery)
+Simple jQuery example demonstrating DOM manipulation and event handling.
 
-## [Pizza Project](https://github.com/ericapietracupa-pixel/)
+### [Pizza Project](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/Pizza_Project)
+Simple pizza ordering menu with various size and topping selections. It then takes the selections and calculates and displays total cost with a list of all selected options.
 
-## [React Projects](https://github.com/ericapietracupa-pixel/)
+### [React Projects](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/React_Projects)
+Various examples of how to implement and use React and JSX to perform different actions and functions on DOM objects.
 
-## [Tic_Tac_Toe](https://github.com/ericapietracupa-pixel/)
+### [Tic-Tac-Toe](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/TicTacToe)
+Classic game of tic-tac-toe. User takes turns clicking on one of the 9 available boxes to place their 'X', it then uses random number generation to determine computers, 'O', placement. On completion of a row, it draws a green line over the winners boxes and plays a celebration sound, it plays a different sound on cats game when neither player wins.
 
-## [Todo App](https://github.com/ericapietracupa-pixel/)
-
+### [Todo App](https://github.com/ericapietracupa-pixel/JavaScript-Projects/tree/main/todo_app)
+Basic To Do app where the user can add and remove items from a list.
